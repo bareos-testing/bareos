1ad3ac3d725aee204268d93c80ef66770e434062
Philipp Storz <philipp.storz@bareos.com>
2019-11-13 16:42:07 +0100

    ctest: use cdash.bareos.org with https

--- core/CTestConfig.cmake
+++ core/CTestConfig.cmake
@@ -20,8 +20,8 @@
 set(CTEST_PROJECT_NAME "Bareos")
 set(CTEST_NIGHTLY_START_TIME "13:00:00 CET")
 
-set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_METHOD "https")
+set(CTEST_DROP_SITE "cdash.bareos.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=Bareos")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
